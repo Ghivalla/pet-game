25e797708902e06587760d4b95d914835d4c54a5
Ghivalla <ghivalla@gmail.com>
2020-11-30 12:43:44 +0100

    added UI interaction with the buttons

--- src/init.js
+++ src/init.js
@@ -1,10 +1,10 @@
 import gameState from "./gameState";
-
-const TICK_RATE = 3000;
+import { TICK_RATE } from "./constants";
+import initButtons from "./buttons";
 
 function init() {
   let nextTickTime = Date.now();
-
+  initButtons(gameState.handleUserAction);
   function nextAnimationFrame() {
     const now = Date.now();
     if (nextTickTime <= now) {
